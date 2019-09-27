3e5707fb760905484980ec6b687d37f63cafad80
songzihao <szh@xitu.com>
2019-09-27 20:11:13 +0800

    add

---
+++ spring-beans/src/test/java/org/springframework/mytest/MyBeanWiringInfoResolverTest.java
@@ -0,0 +1,19 @@
+package org.springframework.mytest;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author songZiHao
+ * @version 1.0.0
+ * @ClassName MyTestBean.java
+ * @Description TODO
+ * @createTime 2019年09月19日 17:47:00
+ */
+public class MyBeanWiringInfoResolverTest {
+
+	@Configurable()
+	private static class Soap {
+	}
+
+}
